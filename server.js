5d0abf86ae17c8dccc34e3aba021d8d1bb01cb85
ngurahwidi <widi050917@gmail.com>
2024-11-20 13:20:50 +0800

    "create handling request"

--- server.js
+++ server.js
@@ -2,14 +2,30 @@ const http = require('http');
 
 const requestListener = (req, res) => {
     res.setHeader('content-type', 'text/html');
-
     res.statusCode = 200;
-    res.end('<h1>Hello World!</h1>');
+
+    const { method } = req;
+
+    if (method === 'GET') {
+        res.end('<h1>Hello</h1>');
+    }
+
+    if (method === 'POST') {
+        res.end('<h1>Hai</h1>');
+    }
+
+    if (method === 'PUT') {
+        res.end('<h1>Bonjour</h1>');
+    }
+
+    if (method === 'DELETE') {
+        res.end('<h1>Salam</h1>');
+    }
 };
 
 const server = http.createServer(requestListener);
 
-const port = 5000;
+const port = 3000;
 const host = 'localhost';
 
 server.listen(port, host, () => {
