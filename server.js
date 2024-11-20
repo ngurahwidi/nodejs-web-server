f35c3208e18f47abec5973a600f586b2cede1508
ngurahwidi <widi050917@gmail.com>
2024-11-20 12:00:34 +0800

    "create request listener and server listen"

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log('hallo')
+const http = require('http');
+
+const requestListener = (req, res) => {
+    res.setHeader('content-type', 'text/html');
+
+    res.statusCode = 200;
+    res.end('<h1>Hello World!</h1>');
+};
+
+const server = http.createServer(requestListener);
+
+const port = 5000;
+const host = 'localhost';
+
+server.listen(port, host, () => {
+    console.log(`Listening on http://${host}:${port}`);
+})
