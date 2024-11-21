0f12a1fcfb310a49cf21321325b509016b0f8dcb
ngurahwidi <widi050917@gmail.com>
2024-11-21 12:24:02 +0800

    "create response status"

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const http = require('http');
 
 const requestListener = (req, res) => {
     res.setHeader('content-type', 'text/html');
-    res.statusCode = 200;
 
     const { method, url } = req;
 
     if (url === '/') {
         if (method === 'GET') {
+            res.statusCode = 200;
             res.end('<h1>Ini adalah homepage</h1>');
         } else {
+            res.statusCode = 400;
             res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
         }
     } else if (url === '/about') {
         if (method === 'GET') {
+            res.statusCode = 200;
             res.end('<h1>Halo, ini adalah halaman about</h1>');
         } else if (method === 'POST') {
             let body = [];
@@ -25,14 +27,17 @@ const requestListener = (req, res) => {
             req.on('end', () => {
                 body = Buffer.concat(body).toString();
                 const { name } = JSON.parse(body);
+                res.statusCode = 200;
                 res.end(`<h1>Halo, ${name}, ini adalah halaman about!</h1>`);
             })
         } else {
+            res.statusCode = 400;
             res.end(`<h1>Halaman tidak dapat diakses dengan ${method} request!</h1>`);
         }
 
     } else {
-        res.end('<h1>Halaman tidak ditemuka</h1>')
+        res.statusCode = 404;
+        res.end('<h1>Halaman tidak ditemukan</h1>')
     }
 };
 
